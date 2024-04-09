f899602ec292e51327d4e0604515039adbfb70ca
JMeteor <jerzy.zientek@gmail.com>
2024-04-09 16:21:05 +0200

    Add env variable for enabling msw

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './i18n';
 import App from './App.tsx';
-// import { setupWorker } from 'msw/browser';
-// import { handlers } from './mocks/handlers.ts';
 
-// if (true) {
-//   setupWorker(...handlers);
-// }
+async function enableMocking() {
+  if (import.meta.env.MODE !== 'development') {
+    return;
+  }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+  const { worker } = await import('./mocks/browser');
+
+  return worker.start();
+}
+
+enableMocking()
+  .then(() => {
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    );
+  })
+  .catch((error) => {
+    console.error('Error while enabling mocks:', error);
+  });
